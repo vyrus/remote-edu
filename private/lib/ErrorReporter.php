02ba51fcdaab2503dff46706b395548a1f740b5e
leon4ik.ru <leon4ik.ru@8be755f0-7b69-014b-b4a2-75d5e90fc6ab>
2009-07-29 20:42:30 +0000

    [*] Убраны short-tag'и в ErrorReporter.
    git-svn-id: http://subversion.assembla.com/svn/remote-edu/trunk@25 8be755f0-7b69-014b-b4a2-75d5e90fc6ab

--- private/lib/ErrorReporter.php
+++ private/lib/ErrorReporter.php
@@ -59,7 +59,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title><?= $header ?></title>
+<title><?php echo $header ?></title>
 <style type="text/css" media="screen">
     html {margin:0;padding:0;background:#BEC6C6;}
     body {width:50em; margin:2em auto;border:4px solid #eee; padding:0.5em 1em 1.5em; font-size:14px; font-family: Verdana, sans-serif; background-color:#fffafa}
@@ -75,10 +75,10 @@
 </style>
 </head>
 <body>
-<h1><?= $header ?></h1>
-<p><?php echo ($exception ? $exception . ' thrown' : 'An error encountered'); ?> in <span class="path"><?= $file ?></span> on line <span class="line"><?= $line ?>.</span></p>
+<h1><?php echo $header ?></h1>
+<p><?php echo ($exception ? $exception . ' thrown' : 'An error encountered'); ?> in <span class="path"><?php echo $file ?></span> on line <span class="line"><?php echo $line ?>.</span></p>
 <h2>Error Message</h2>
-<p><?= $message ?>.</p>
+<p><?php echo $message ?>.</p>
 <?php 
 
 $show_backtrace = true;
@@ -91,7 +91,7 @@ $trace = array_slice($trace, $skip_backtrace);
 <?php    foreach ( $trace as $line ): ?>
 <?php     if ( isset($line['function']) && $line['function'] == 'trigger_error' ) continue; ?>
     <li>
-        <span class="path"><?= isset($line['file']) ? $line['file'] .':' : 'PHP inner process:' ?></span>
+        <span class="path"><?php echo isset($line['file']) ? $line['file'] .':' : 'PHP inner process:' ?></span>
         <div class="code-block">
         <?php
             echo '<span class="line">' . ( isset($line['line']) ? $line['line'] . '.' : '') . '</span>';
