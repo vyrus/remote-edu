37827020e021c88567c138a13231ff575924a16b
nadya <nadya@8be755f0-7b69-014b-b4a2-75d5e90fc6ab>
2009-10-18 20:11:38 +0000

    получение данных о выбранном направлении/курсе или дисциплине от браузера при подаче заявки
    git-svn-id: http://subversion.assembla.com/svn/remote-edu/trunk@72 8be755f0-7b69-014b-b4a2-75d5e90fc6ab

--- private/app/Controller/Applications.php
+++ private/app/Controller/Applications.php
@@ -5,29 +5,40 @@
     class Controller_Applications extends Mvc_Controller_Abstract {
         const RETURN_URL = '/applications/index/';
         
-        public function action_index() {
+        public function action_index()
+        {
+			$educationPrograms = Model_Education_Programs::create ();
+			$this->set ('directions',	$educationPrograms->getDirections 				());
+			$this->set ('courses', 		$educationPrograms->getCourses 					());
+			$this->set ('disciplines',	$educationPrograms->getDirectionsDisciplines 	());
+
+            /* Создаём объект формы с полями первичной регистрации */
+            $action = '/applications/apply/';
+            $form = Form_Profile_Student_Registration::create($action);
+            $this->set('form', $form);
+			
             $this->render();
         }
         
         /**
         * Подача заявки на программу/дисциплину.
         */
-        public function action_apply() {
+        public function action_apply($params) {
             $user = Model_User::create();
             $udata = (object) $user->getAuth();
-            
+/*раскоментировать когда поправится подключение js-функций, подгружающих списки городов/областей
             if (!$user->isExtendedProfileSet($udata->user_id))
             {
                 $msg = 'Заполните, пожалуйста, свой профиль';
                 $link = '/users/profile_extended/';
                 $this->flash($msg, $link);
             }
-            
+*/
             /**
-            * @todo Исправить на получение данных от браузера.
+            * Получение данных от браузера.
             */
-            $object_id = 2;
-            $type = Model_Application::TYPE_DISCIPLINE;
+            $object_id = $params[1];
+            $type = $params[0];
             
             $app = Model_Application::create();       
             $app->apply($udata->user_id, $object_id, $type);
@@ -39,16 +50,26 @@
         * Просмотр статуса заявок слушателем.
         */
         public function action_list() {
+            
+            /* Управление прходит сюда после диспетчера */
+            
             $user = Model_User::create();
             $udata = (object) $user->getAuth();
+            
+            /* Выполняем здесь какие-нибудь общественно-полезные действия =)*/
             
             /**
             * @todo Paginator.
             */
             $app = Model_Application::create();
             $apps = $app->getAppsInfo($udata->user_id);
+            
+            /* По ходу дейтсвия формируем набор переменных для шаблона */
             $this->set('applications', $apps);
             
+            $this->set('test', 'true');
+            
+            /* А потом уже вызываем рендеринг самого шаблона */
             $this->render();
         }
     }
