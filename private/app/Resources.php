6bd4212fb7adee247ff928f7966040ea51222cc4
leon4ik.ru <leon4ik.ru@8be755f0-7b69-014b-b4a2-75d5e90fc6ab>
2010-03-14 19:09:35 +0000

    [*] uncomment line #59 in private/app/Resources.php ($db->exec('SET NAMES utf8');) [*] remove some extra spaces
    git-svn-id: http://subversion.assembla.com/svn/remote-edu/trunk@187 8be755f0-7b69-014b-b4a2-75d5e90fc6ab

--- private/app/Resources.php
+++ private/app/Resources.php
@@ -1,14 +1,14 @@
 <?php
 
     /* $Id$ */
-    
+
     /**
     * Класс менеджера ресурсов приложения.
     */
     class Resources extends Resources_Abstract {
         /**
         * Создание экземпляра класса.
-        * 
+        *
         * @param  array $config Массив с конфигурацией.
         * @return Resources
         */
@@ -16,80 +16,80 @@
             $instance = new self($config);
             return self::setInstance($instance);
         }
-        
+
         /**
         * Инициализация диспетчера запросов.
-        * 
+        *
         * @return Mvc_Dispatcher
         */
         protected function get_dispatcher() {
             $config = $this->_config['permissions'];
-            
+
             $dispatcher = Mvc_Dispatcher::create($config);
-            
+
             return $dispatcher;
         }
-        
+
         /**
         * Инициализация роутера.
-        * 
+        *
         * @return Mvc_Router
         */
         protected function get_router() {
             $config = $this->_config['routes'];
-            
+
             $router = Mvc_Router::create($config);
-            
+
             return $router;
         }
-        
+
         /**
         * Инициализация объекта для работы с БД.
-        * 
+        *
         * @return Db_Pdo
         */
         protected function get_db() {
             $config = $this->_config['db'];
-            
+
             $db = Db_Pdo::create(
                 $config['dsn'], $config['user'], $config['passwd'],
                 $config['options']
             );
-            
-            //$db->exec('SET NAMES utf8');
-            
+
+            $db->exec('SET NAMES utf8');
+
             return $db;
         }
-        
+
         /**
         * Инициализация объекта авторизации.
-        * 
+        *
         * @return Auth
         */
         protected function get_auth() {
             $config = $this->_config['auth'];
-            
+
             $auth = Auth::create($config['salt']);
-            
-            return $auth;    
+
+            return $auth;
         }
-        
+
         /**
         * Инициализация объекта рассылки почтовых сообщений.
-        * 
+        *
         * @return Postman
         */
         protected function get_postman() {
             $config = $this->_config['postman'];
-            
-            $postman = Postman::create($this->_config['base_url'], $config);   
-            
-            return $postman; 
+
+            $postman = Postman::create($this->_config['base_url'], $config);
+
+            return $postman;
         }
-        
+
         /**
         * Инициализация обработчика шаблонов.
-        * 
+        *
         * @return Mvc_View
         */
         protected function get_view() {
@@ -97,5 +97,5 @@
             return $view;
         }
     }
-        
+
 ?>
--- private/app/Storage.php
+++ private/app/Storage.php
@@ -1,38 +1,38 @@
 <?php
-	class Storage {
-		private $directory;
-		
-		public function __construct ($directory) {
-			$this->directory = $directory;
-		}
-		
-		public function storeFile () {
-			$args = func_get_args();
-			$path = array_shift($args);
-			if (count($args)) {
-				$filename = array_shift($args);
-			}
-			else {
-				do {
-					$filename = md5($path . rand (0, 0xFFFFFF));
-				} while (file_exists($this->directory . '/' . $filename)); 
-			}
-			
-			if (@move_uploaded_file($path, $this->directory . '/' . $filename) === FALSE) {
-				throw new Exception ('Невозможно сохранить файл на сервере');
-			}
-			
-			return $filename;
-		}
-		
-		public function removeFile ($filename) {
-			if (@unlink ($this->directory . '/' . $filename) === FALSE) {
-				throw new Exception ('Невозможно удалить файл с сервера');
-			}
-		}
-		
-		public function getFileContent ($filename) {
-			return file_get_contents ($this->directory . '/' . $filename);
-		}
-	}
+    class Storage {
+        private $directory;
+
+        public function __construct ($directory) {
+            $this->directory = $directory;
+        }
+
+        public function storeFile () {
+            $args = func_get_args();
+            $path = array_shift($args);
+            if (count($args)) {
+                $filename = array_shift($args);
+            }
+            else {
+                do {
+                    $filename = md5($path . rand (0, 0xFFFFFF));
+                } while (file_exists($this->directory . '/' . $filename));
+            }
+
+            if (@move_uploaded_file($path, $this->directory . '/' . $filename) === FALSE) {
+                throw new Exception ('Невозможно сохранить файл на сервере');
+            }
+
+            return $filename;
+        }
+
+        public function removeFile ($filename) {
+            if (@unlink ($this->directory . '/' . $filename) === FALSE) {
+                throw new Exception ('Невозможно удалить файл с сервера');
+            }
+        }
+
+        public function getFileContent ($filename) {
+            return file_get_contents ($this->directory . '/' . $filename);
+        }
+    }
 ?>
